232f4978fc165a53f75adc7ff07c75cf1bf0e1ad
askndaer1 <askndaer@gmail.com>
2017-07-11 08:01:21 +0800

    With Noti

--- app/src/main/java/com/laundromat/codesoft/laundromat/Login.java
+++ app/src/main/java/com/laundromat/codesoft/laundromat/Login.java
@@ -2,42 +2,109 @@ package com.laundromat.codesoft.laundromat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.laundromat.codesoft.laundromat.Laundromast.*;
 
 public class Login extends Activity {
 
-    Button Login_Register_BTN,Login_Login_BTN;
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+    private Button btnSignup, btnLogin, btnReset;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
 
-        Login_Register_BTN = (Button) findViewById(R.id.Login_Register_BTN);
+        btnSignup = (Button) findViewById(R.id.Login_Register_BTN);
 
-        Login_Register_BTN.setOnClickListener(new View.OnClickListener() {
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        btnLogin = (Button) findViewById(R.id.Login_Login_BTN);
+
+
+        inputEmail = (EditText) findViewById(R.id.Login_Email);
+        inputPassword = (EditText) findViewById(R.id.Login_Password);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Login.this,LaundromatList.class);
-                startActivity(i);
+                startActivity(new Intent(Login.this, Register.class));
             }
         });
 
+//        btnReset.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+//            }
+//        });
 
-
-        Login_Login_BTN = (Button) findViewById(R.id.Login_Login_BTN);
-
-        Login_Login_BTN.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Login.this,MainActivity.class);
-                startActivity(i);
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        inputPassword.setError("Password too short, enter minimum 6 characters!");
+                                    } else {
+                                        Toast.makeText(Login.this, "Authentication failed, check your email and password or sign up", Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(Login.this, LaundromatList.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
     }
-}
+
+
+
+    }
+
